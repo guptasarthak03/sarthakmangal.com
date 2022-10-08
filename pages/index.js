4d1d4392ad9dd2174fbbcba399749773deed206c
Sarthak gupta <guptasarthak03@gmail.com>
2022-10-08 14:34:50 +0530

    Feat: services and skills

--- pages/index.js
+++ pages/index.js
@@ -39,6 +39,77 @@ const Home = () => {
             <div className="email-handle">sarthakMangal.com</div>
           </div>
         </header>
+        <section className="section-services">
+          <p className="section-header">What I Do ?</p>
+          <div className="my-services">
+            <div className="service">
+              <img src="/images/ui-ux.svg" alt="UI-UX" />
+              <p class="service-heading">UI & UX Design</p>
+              <p>
+                I enjoy creating eye-pleasing, user centeric UI with an essence
+                of minimalistic design.
+              </p>
+            </div>
+            <div className="service">
+              <img src="/images/web-development.svg" alt="Web Development" />
+              <p class="service-heading">Web Development</p>
+              <p>
+                I am a front-end developer who writes clean and optimized code
+                with mobile-first approach in mind.
+              </p>
+            </div>
+            <div className="service">
+              <img src="/images/performance-seo.svg" alt="Performance & SEO" />
+              <p class="service-heading">Performance & SEO</p>
+              <p>
+                Proven experience in improving speed & metrics of websites,
+                making it stand out from the crowd.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="section-skills">
+          <p className="section-header">What I Know ?</p>
+          <div className="skill-items">
+            <div className="skill-item">
+              <img src="/images/nextjs-icon.svg" alt="next-js" />
+              <p>Next.JS</p>
+            </div>
+            <div class="skill-item">
+              <img src="/images/react-icon.svg" alt="react-js" />
+              <p>React.JS</p>
+            </div>
+            <div class="skill-item">
+              <img src="/images/redux.svg" alt="redux" />
+              <p>Redux</p>
+            </div>
+            <div class="skill-item">
+              <img src="/images/html5.svg" alt="next-js" />
+              <p>HTML</p>
+            </div>
+            <div class="skill-item">
+              <img src="/images/css3.svg" alt="css3" />
+              <p>CSS</p>
+            </div>
+          </div>
+          <div class="divider-hr"></div>
+          <div class="skill-items">
+            <div class="skill-item">
+              <img src="/images/version-control.svg" alt="version-control" />
+              <p>Version Control</p>
+            </div>
+            <div class="skill-item">
+              <img src="/images/agile.svg" alt="Agile" />
+              <p>Agile Methodology</p>
+            </div>
+          </div>
+        </section>
+        <section className="section-projects">
+          <p className="section-header">What I've been up to ?</p>
+          <div className="project-items">
+            <div className="project-item"></div>
+          </div>
+        </section>
       </div>
     </div>
   );
