90146e4faf37c0a766b942e5594863ac2ddddc70
Sarthak gupta <guptasarthak03@gmail.com>
2022-10-14 07:46:26 +0530

    Feat: work headers & view more button

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,12 @@ import Navbar from '../components/Navbar';
 import SocialAccount from '../components/common/SocialAccount';
 import ServicesContainer from '../components/container/servicesContainer';
 import SkillListContainer from '../components/container/skillListContainer';
+import { useState } from 'react';
+import Link from 'next/link';
 
 const Home = () => {
+  const [workHeader, setWorkHeader] = useState('portfolio');
+
   // custom inline style for icon
   const projectIconStyleConfig = {
     width: '2.3rem',
@@ -59,44 +63,89 @@ const Home = () => {
         </section>
         <section className="section-skills">
           <p className="section-header">What I Know ?</p>
-          <SkillListContainer partition={1} />
-          <div className="divider-hr" />
-          <SkillListContainer partition={2} />
+          <div className="my-skills">
+            <SkillListContainer partition={1} />
+            <div className="divider-hr" />
+            <SkillListContainer partition={2} />
+          </div>
         </section>
-        <section className="section-projects">
+        <section className="section-projects-blogs">
           <p className="section-header">What I've been up to ?</p>
-          <div className="projects-items">
-            <div
-              className="projects-item"
-              style={{ backgroundImage: `url(/images/project-1.jpg)` }}
-            >
-              <div className="content">
-                <p className="heading">Banana Language</p>
-                <p className="description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-                  eveniet provident.
-                </p>
-                <div className="projects-cta">
-                  <SocialAccount
-                    source="https://www.linkedin.com/in/sarthak-gupta-b64480102/"
-                    name="code"
-                    isWhite={true}
-                    styleConfig={projectIconStyleConfig}
-                    hoverConfig={projectIconHoverConfig}
-                  />
-                  <SocialAccount
-                    source="https://www.linkedin.com/in/sarthak-gupta-b64480102/"
-                    name="preview"
-                    isWhite={true}
-                    styleConfig={projectIconStyleConfig}
-                    hoverConfig={projectIconHoverConfig}
-                  />
+          <div className="my-work">
+            <div className="header-tab">
+              <div
+                className="button-active"
+                style={
+                  workHeader == 'blog' ? { left: '130px', width: '128px' } : {}
+                }
+              ></div>
+              <button
+                className={
+                  workHeader == 'portfolio' ? 'work-header-active' : ''
+                }
+                onClick={() => setWorkHeader('portfolio')}
+              >
+                My Portfolio
+              </button>
+              <button
+                className={workHeader == 'blog' ? 'work-header-active' : ''}
+                onClick={() => setWorkHeader('blog')}
+              >
+                My Blogs
+              </button>
+            </div>
+            <div className="my-projects">
+              <div className="projects-items">
+                <div
+                  className="projects-item"
+                  style={{ backgroundImage: `url(/images/project-1.jpg)` }}
+                >
+                  <div className="content">
+                    <p className="heading">Banana Language</p>
+                    <p className="description">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Minus eveniet provident.
+                    </p>
+                    <div className="projects-cta">
+                      <SocialAccount
+                        source="https://www.linkedin.com/in/sarthak-gupta-b64480102/"
+                        name="code"
+                        isWhite={true}
+                        styleConfig={projectIconStyleConfig}
+                        hoverConfig={projectIconHoverConfig}
+                      />
+                      <SocialAccount
+                        source="https://www.linkedin.com/in/sarthak-gupta-b64480102/"
+                        name="preview"
+                        isWhite={true}
+                        styleConfig={projectIconStyleConfig}
+                        hoverConfig={projectIconHoverConfig}
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="my-blogs"></div>
+            <div className="view-more">
+              <Link href="/">
+                <a>View More</a>
+              </Link>
+            </div>
           </div>
         </section>
-        <section className="section-blogs">
+
+        <section></section>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
+// My Work & My Thoughts
+{
+  /* <section className="section-blogs">
           <p className="section-header">My Blogs</p>
           <div className="blog-items">
             <div className="blog-item">
@@ -106,12 +155,5 @@ const Home = () => {
               </div>
             </div>
           </div>
-        </section>
-      </div>
-    </div>
-  );
-};
-
-export default Home;
-
-// My Work & My Thoughts
+        </section> */
+}
